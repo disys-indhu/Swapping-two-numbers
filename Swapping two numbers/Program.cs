ee1140db3846d4e0160c7a7f6e4740eb9dcdc94a
indhumathi <Indhumathi.Ramesh@disys.com>
2022-02-11 06:59:20 -0800

    Add project files.

---
+++ Swapping two numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Swapping
+{
+    static void Main(string[] args)
+    {
+        int first, second;
+        Console.Write("Enter the First Number: ");
+        first = int.Parse(Console.ReadLine());
+        Console.Write("Enter the Second Number: ");
+        second = int.Parse(Console.ReadLine());
+        if (first != second)
+        {
+            first = first + second;
+            second = first - second;
+            first = first - second;
+            Console.WriteLine("The First Number after Swapping: " + first);
+            Console.Write("The Second Number after swapping: " + second);
+        }
+        else
+            Console.WriteLine("The First and Second Number should be different");
+    }
+}
+
